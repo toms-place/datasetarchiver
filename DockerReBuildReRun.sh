d375a769825d7d42af19a96087c4aeffeb96fa9b
Thomas Weber <thomas.weber96@gmail.com>
2019-08-29 21:40:26 +0200

    update

--- DockerReBuildReRun.sh
+++ DockerReBuildReRun.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker container stop --filter "label=datasetarchiver"
+docker container stop $(docker container ls -f name=datasetarchiver)
 docker rmi datasetarchiver/crawler datasetarchiver/master --force
 docker build --no-cache=true -t datasetarchiver/crawler .
 docker build --no-cache=true -t datasetarchiver/master -f Dockerfile_master .
-docker rmi (docker images -f “dangling=true” -q )
-docker run --network="host" -l datasetarchiver -d datasetarchiver/crawler
-docker run --network="host" -l datasetarchiver -d datasetarchiver/master
+docker rmi $(docker images -f dangling=true -q)
+docker run --network="host" --rm -l datasetarchiver -d datasetarchiver/crawler
+docker run --network="host" --rm -l datasetarchiver -d datasetarchiver/master
