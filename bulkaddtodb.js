86440ff18b5c7834fa43078e935ea8aead1b67b3
Thomas Weber <thomas.weber96@gmail.com>
2019-08-29 17:20:58 +0200

    insertMany

--- bulkaddtodb.js
+++ bulkaddtodb.js
@@ -9,17 +9,32 @@ const {
 
 db.connect().then(() => {
 
+	let results = [];
+
 	for (let i = 0; i <= 24; i++) {
 
-		let results = [];
 		fs.createReadStream(`./europeandataportal/${i}.csv`)
 			.pipe(csv())
 			.on('data', (data) => results.push(data))
 			.on('end', async () => {
-				await addManyHrefsToDB(results)
 				if (i == 24) {
+					await bulk(results)
 					process.exit()
 				}
 			})
 	}
 });
+
+async function bulk(results) {
+	let bulk = [];
+	for (let i = 0; i < results.length; i++) {
+		bulk.push(results[i])
+		if (i%1000 == 0) {
+			await addManyHrefsToDB(bulk)
+			bulk = []
+		} else if (i == results.length - 1) {
+			await addManyHrefsToDB(bulk)
+			return
+		}
+	}
+}
