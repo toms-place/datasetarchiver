ef8ee841d993cd32dc5ca149a28b5449a0d225d5
Thomas Weber <thomas.weber96@gmail.com>
2019-08-29 21:28:44 +0200

    none image delete

--- DockerReBuildReRun.sh
+++ DockerReBuildReRun.sh
@@ -2,7 +2,7 @@
 
 docker container prune --filter "label=datasetarchiver"
 docker rmi datasetarchiver/crawler datasetarchiver/master --force
-docker build --no-cache=true -t datasetarchiver/crawler .
-docker build --no-cache=true -t datasetarchiver/master -f Dockerfile_master .
+docker build --no-cache=true --rm -t datasetarchiver/crawler .
+docker build --no-cache=true --rm -t datasetarchiver/master -f Dockerfile_master .
 docker run --network="host" -l datasetarchiver -d datasetarchiver/crawler
 docker run --network="host" -l datasetarchiver -d datasetarchiver/master
